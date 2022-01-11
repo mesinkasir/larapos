9cadde11e13651760d70f3620dab14123bf5682b
Mesin Kasir <52827888+mesinkasir@users.noreply.github.com>
2022-01-11 19:35:30 +0700

    Create gallery.js

---
+++ pos/gallery.js
@@ -0,0 +1,4 @@
+var app = new Vue({ 
+    el: '#app',
+});
+document.write( '<center><p><small><small>made with ❤ <a href=\'https://axcora.com/getaxcora.cms\' class=\'text-danger\' target=\'blank\'>get axcora cms</a></p></center>' );
