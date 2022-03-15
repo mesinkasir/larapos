2bb293d23e218ee3515b746db6244a15005fe5b3
Mesin Kasir <52827888+mesinkasir@users.noreply.github.com>
2022-03-15 12:44:58 +0700

    Create cashierax.js

---
+++ pos/cashierax.js
@@ -0,0 +1,29 @@
+	$('#login-form').submit(function(e){
+		e.preventDefault()
+		$('#login-form button[type="button"]').attr('disabled',true).html('Logging in...');
+		if($(this).find('.alert-danger').length > 0 )
+			$(this).find('.alert-danger').remove();
+		$.ajax({
+			url:'ajax.php?action=login',
+			method:'POST',
+			data:$(this).serialize(),
+			error:err=>{
+				console.log(err)
+		$('#login-form button[type="button"]').removeAttr('disabled').html('Login');
+
+			},
+			success:function(resp){
+				if(resp == 1){
+					location.href ='index.php?page=home';
+				}else{
+					$('#login-form').prepend('<div class="alert alert-danger">Username or password is incorrect.</div>')
+					$('#login-form button[type="button"]').removeAttr('disabled').html('Login');
+				}
+			}
+		})
+	})
+	$('.number').on('input',function(){
+        var val = $(this).val()
+        val = val.replace(/[^0-9 \,]/, '');
+        $(this).val(val)
+    })
